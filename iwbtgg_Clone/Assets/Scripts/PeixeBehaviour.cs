9298898d6cb7f095da131f647303c1dd72ba6e01
skydeveloperoneoficial <developerskyone@gmail.com>
2020-03-05 23:32:24 -0300

    Comit1
    inicio

---
+++ iwbtgg_Clone/Assets/Scripts/PeixeBehaviour.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+using UnityEngine.UI;
+
+public class PeixeBehaviour : MonoBehaviour
+{
+    public  float  speed;
+    private Vector3 pos;
+    public bool MoveFish;
+    public  Text gameOver;
+    public bool enableGravity= false;
+   [SerializeField] private  float timeToDestroy;
+    public SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private Platformer2DUserControl robot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = FindObjectOfType(typeof(Text)) as Text;
+        robot = FindObjectOfType(typeof(Platformer2DUserControl)) as Platformer2DUserControl;
+        gameOver.text = "GameOver";
+        gameOver.gameObject.SetActive(false);
+        MoveFish = false;
+       spriteRenderer = FindObjectOfType(typeof(SpriteRenderer))as SpriteRenderer;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(MoveFish)
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+    }
+    private void FixedUpdate()
+    {
+        if(enableGravity)
+        rb.gravityScale = 1;
+        
+        
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            gameOver.gameObject.SetActive(true);
+            
+            robot.gameObject.SetActive(false);
+            Destroy(gameObject, timeToDestroy);
+
+        }
+        else
+        {
+            
+            enableGravity = true;
+            
+
+
+        }
+        
+    }
+
+
+
+
+}
---
+++ iwbtgg_Clone/Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets._2D;
+public class TriggerBehaviour : MonoBehaviour
+{
+    private PeixeBehaviour peixeBehaviour;
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+        peixeBehaviour = FindObjectOfType(typeof(PeixeBehaviour)) as PeixeBehaviour;
+        peixeBehaviour.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            peixeBehaviour.MoveFish = true;
+            peixeBehaviour.gameObject.SetActive(true);
+            
+            
+        }
+    }
+    
+}
